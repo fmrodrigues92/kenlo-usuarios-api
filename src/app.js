c0c4eb88f41e1303dc279108e1263e31027f8821
Fernando Menezes <fmrodrigues92@yahoo.com>
2024-04-16 15:05:15 -0300

    test

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error(err));
 
 app.get('/', (req, res) => res.send('Usuário API - OK'));
 
